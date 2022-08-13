93cefe451918273c9dc77368893915fe3b65d0c4
yh <yuukihigaki@hgaiji.local>
2022-08-13 10:09:55 +0900

    notion insert batch処理を追加

--- src/insert_notion.py
+++ src/insert_notion.py
@@ -9,19 +9,18 @@ from model.tech_site_notiondb import TechSiteNotionDB
 
 tech_sites = [
     {"site_name": "qiita", "object": QiitaTrend()},
-    # {"site_name": "zenn", "object": ZennTrend()},
-    # {"site_name": "devto", "object": DevtoTrend()},
+    {"site_name": "zenn", "object": ZennTrend()},
+    {"site_name": "devto", "object": DevtoTrend()},
 ]
 
 notiondb_client = TechSiteNotionDB()
-# .insert("test", "test", "http://site-nam.com", "http://example.com")
+
 for tech_site_info in tech_sites:
     try:
         tech_site_name = tech_site_info["site_name"]
         tech_site = tech_site_info["object"]
 
         trend_results = tech_site.fetch()
-        print(trend_results)
         for trend_result in trend_results:
             twi_url = TwitterScraper.scraping(
                 url=trend_result["user_url"], ignore=tech_site_name
@@ -33,7 +32,6 @@ for tech_site_info in tech_sites:
                 user_url=trend_result["user_url"],
                 twitter_url=twi_url or "None",
             )
-            time.sleep(1)
-            exit(1)
+            time.sleep(0.5)
     except Exception as e:
         print(e)
--- src/model/twitter_scraper.py
+++ src/model/twitter_scraper.py
@@ -7,16 +7,27 @@ from .scraping_base import ScrapingBase
 
 
 class TwitterScraper(ScrapingBase):
-    def scraping(url: str, ignore=["qiita", "zenn"]):
+    def scraping(url: str, ignore="qiita"):
         gh_trend_page = requests.get(url)
 
         # Create a BeautifulSoup object
         soup = BeautifulSoup(gh_trend_page.text, "html.parser")
         a_tags = soup.find(
-            # TODO: ignore引数に応じて除外条件を設定できるようにする
             "a",
-            attrs={"href": re.compile(r"https://twitter.com/[^qiita | ^zenn].+")},
+            attrs={"href": eval(f"_{ignore}_recomplie")()},
         )
         if a_tags is None:
             return None
         return a_tags.get("href", None)
+
+
+def _qiita_recomplie():
+    return re.compile(r"https://twitter.com/[^qiita].+")
+
+
+def _zenn_recomplie():
+    return re.compile(r"https://twitter.com/[^zenn].+")
+
+
+def _devto_recomplie():
+    return re.compile(r"https://twitter.com/[^thepracticaldev].+")
